49b053fe2b87181fb8f2b846337ece5eb531374d
munhyebin <munhyebin@DESKTOP-4N70LS6>
2023-06-29 18:36:58 +0900

    초기 커밋.

---
+++ CPP_2023/참소변수.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std; // 이름공간
+
+int main() {
+
+	// std::cout : 출력을 위한 객체
+	// << : 출력연산자
+	// std::endl : 개행(줄바꿈) 객체
+
+	int a = 2023;
+	cout << "Hello World" << a << endl;
+
+	return 0;
+}
